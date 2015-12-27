67d7815220a3882bde49bf6e569ceff055428398
Ajay V <ajayveee@gmail.com>
2015-12-26 22:47:40 -0500

    reorgannized packages

---
+++ src/wordcount/heap/WordComparator.java
@@ -0,0 +1,21 @@
+package wordcount.heap;
+import java.util.Comparator;
+
+import wordcount.Word;
+
+
+public class WordComparator implements Comparator<Word>{
+
+	@Override
+	public int compare(Word o1, Word o2) {
+		// TODO Auto-generated method stub
+//		if(o1.getOccurance()>o2.getOccurance())
+//			return 1;
+//		else if(o1.getOccurance()<o2.getOccurance())
+//			return -1;
+//		else if(o1.getOccurance() == o2.getOccurance())
+//			return 0;
+		return o2.getOccurance() - o1.getOccurance();
+	}
+
+}
